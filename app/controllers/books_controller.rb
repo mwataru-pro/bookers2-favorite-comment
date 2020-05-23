631f27811f783155ec389d6ec0ac7c878c5dfec6
mwataru-pro <m1130wata@gmail.com>
2020-05-23 16:49:26 +0900

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user! # :authenticate_user!とすることによって、「ログイン認証されていなければ、ログイン画面へリダイレクトする」機能を実装
+  before_action :baria_user, only:[:edit,:update]
+
+  def show
+  	@book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
+  end
+
+  def index
+    @book = Book.new
+  	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+  end
+
+  def create
+  	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @book.user_id = current_user.id
+  	if @book.save #入力されたデータをdbに保存する。
+  		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
+  	else
+  		@books = Book.all
+  		render 'index'
+  	end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+
+
+  def update
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  		redirect_to @book, notice: "successfully updated book!"
+  	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
+  		render "edit"
+  	end
+  end
+
+  def delete
+  	@book = Book.find(params[:id])
+  	@book.destoy
+  	redirect_to books_path, notice: "successfully delete book!"
+  end
+
+  private
+  def baria_user
+    book = Book.find(params[:id])
+    if book.user != current_user
+      redirect_to books_path
+    end
+  end
+
+  def book_params
+  	params.require(:book).permit(:title,:body)
+  end
+
+end
