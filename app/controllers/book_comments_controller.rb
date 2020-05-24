b60aae24a1abd395e92a635562900a6cf5dffdf1
mwataru-pro <m1130wata@gmail.com>
2020-05-24 21:48:00 +0900

    [Add] コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,28 @@
 class BookCommentsController < ApplicationController
+	before_action :authenticate_user!
 
 	def create
-		
+		@book = Book.find(params[:book_id])
+		# 下記記2つの記述はbook_comment = current_user.book_comments.new(book_comment_params)の記述でまとめられる。
+		@book_comment = BookComment.new(book_comment_params)
+		@book_comment.user_id = current_user.id
+		# book_commentsテーブルにしまうbook_idの取得。コメントをする本のID＝本のID(一番上の記述でbookは取得済み。その中のID。)
+		@book_comment.book_id = @book.id
+		# コメントの保存
+		@book_comment.save
+		# 元のページへ遷移
+		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
-		
+		@book = Book.find(params[:book_id])
+		book_comment = @book.book_comments.find(params[:id])
+		book_comment.destroy
+		redirect_back(fallback_location: root_path)
+	end
+
+	private
+	def book_comment_params
+		params.require(:book_comment).permit(:comment)
 	end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
 
   def show
   	@book = Book.find(params[:id])
-    @user = User.find(@book.user_id)
+    @book_comment = BookComment.new
+    @book_comments = @book.book_comments
   end
 
   def index
